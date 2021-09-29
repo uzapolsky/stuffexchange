71b9a5b22f73da296ffeea43b19f6bb147419113
uzapolsky <uzapolsky@gmail.com>
2021-09-29 09:18:41 +0300

    delete STATICFILESS_DIRS to avoid collectstatic mistakes

--- stuffexchange/settings.py
+++ stuffexchange/settings.py
@@ -94,10 +94,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
