aae57845c7fcdb804a8996bae081d9da4b8a3d19
uzapolsky <uzapolsky@gmail.com>
2021-10-01 12:36:59 +0300

    add right cloudinary settings for both local and server developing

--- stuffexchange/settings.py
+++ stuffexchange/settings.py
@@ -108,4 +108,12 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+if env('CLOUDINARY', False):
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': env('CLOUD_NAME'),
+        'API_KEY': env('API_KEY'),
+        'API_SECRET': env('API_SECRET'),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 django_heroku.settings(locals(), databases=False)
